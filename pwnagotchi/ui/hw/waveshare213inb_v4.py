2308582e0b931574a42a813b6bccfe7d273145d3
Leon Twemlow <46332681+SSpiderlxx@users.noreply.github.com>
2023-06-27 22:07:08 +0100

    screen size fix

--- pwnagotchi/ui/hw/waveshare213inb_v4.py
+++ pwnagotchi/ui/hw/waveshare213inb_v4.py
@@ -30,10 +30,30 @@ class Waveshare213bV4(DisplayImpl):
                 'font': fonts.status_font(fonts.Medium),
                 'max': 20
             }
+        if self.config['color'] == 'fastAndFurious':
+            fonts.setup(10, 9, 10, 35, 25, 9)
+            self._layout['width'] = 250
+            self._layout['height'] = 122
+            self._layout['face'] = (0, 40)
+            self._layout['name'] = (5, 20)
+            self._layout['channel'] = (0, 0)
+            self._layout['aps'] = (28, 0)
+            self._layout['uptime'] = (185, 0)
+            self._layout['line1'] = [0, 14, 250, 14]
+            self._layout['line2'] = [0, 108, 250, 108]
+            self._layout['friend_face'] = (0, 92)
+            self._layout['friend_name'] = (40, 94)
+            self._layout['shakes'] = (0, 109)
+            self._layout['mode'] = (225, 109)
+            self._layout['status'] = {
+                'pos': (125, 20),
+                'font': fonts.status_font(fonts.Medium),
+                'max': 20
+            }
         else:
             fonts.setup(10, 8, 10, 25, 25, 9)
-            self._layout['width'] = 212
-            self._layout['height'] = 104
+            self._layout['width'] = 250
+            self._layout['height'] = 122
             self._layout['face'] = (0, 26)
             self._layout['name'] = (5, 15)
             self._layout['channel'] = (0, 0)
